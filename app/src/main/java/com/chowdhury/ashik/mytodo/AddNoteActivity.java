5e3ffe28266c18c7c0d427c1d6e0a077d17438d8
Ashik Chowdhury <ashikbracu@gmail.com>
2019-08-09 23:37:40 +0600

    Note adapter added and add layout start

---
+++ app/src/main/java/com/chowdhury/ashik/mytodo/AddNoteActivity.java
@@ -0,0 +1,14 @@
+package com.chowdhury.ashik.mytodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+}
